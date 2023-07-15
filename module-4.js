d6714b38b53461ab06d1fb16bdd88869449c329e
GusarovaYuliya <gusarovayuliya25@gmail.com>
2023-07-15 22:57:48 +0300

    comm

---
+++ module-4.js
@@ -0,0 +1,26 @@
+// Task - 1
+// Доповни код таким чином, щоб у змінній result був результат виконання функції makePizza, а у змінній pointer було посилання на функцію makePizza.
+
+// Оголошена функція makePizza
+// Оголошена змінна result
+// Значення змінної result - це рядок "Your pizza is being prepared, please wait."
+// Значення змінної result отримане за допомогою виклику функції
+// Оголошена змінна pointer
+// Значення змінної pointer - це посилання на функцію makePizza
+
+function makePizza() {
+  return "Your pizza is being prepared, please wait.";
+}
+
+const result = (makePizza("Your pizza is being prepared, please wait."));
+const pointer = (makePizza);
+
+// Task - 3
+// Доповни функцію makeMessage таким чином, щоб вона очікувала другим параметром (параметр callback) колбек-функцію і повертала її виклик. Функція deliverPizza або makePizza буде передаватися як колбек і очікувати аргументом ім'я готової піци, що доставляється.
+
+// Оголошена функція deliverPizza
+// Оголошена функція makePizza
+// Оголошена функція makeMessage
+// Функція makeMessage приймає два параметри, названі відповідно до завдання, pizzaName і callback
+// Виклик makeMessage("Royal Grand", makePizza) повертає рядок "Pizza Royal Grand is being prepared, please wait..."
+// Виклик makeMessage("Ultracheese", deliverPizza) повертає рядок "Delivering Ultracheese pizza."
